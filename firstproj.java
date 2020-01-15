6e1b5d933bcdaea6413d3895287fd79c171fa9fe
Aaron C <babyachu835@gmail.com>
2020-01-14 21:20:38 -0800

    firstproj java

---
+++ firstproj.java
@@ -0,0 +1,16 @@
+public class FirstProj
+{ 
+
+	public static void main(String args[]) 
+	{ 
+	    boolean tester = false;
+		for (int i = 10; i<20;i++) {
+			System.out.println("yooo");
+	    }	
+		System.out.println("Hi my name is Aaron, I am currently 13 years old."); 
+		if(tester == false){
+		    System.out.println("oogabooga");
+		    }
+		}
+	 
+} 
