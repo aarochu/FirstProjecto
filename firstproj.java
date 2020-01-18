5b6bbb4bc702e5ac832229f5d3723e28fae652dd
Austin Sun <sunaustin8@gmail.com>
2020-01-18 11:10:14 -0800

    to do problems

--- firstproj.java
+++ firstproj.java
@@ -4,10 +4,12 @@ public class FirstProj
 	public static void main(String args[]) 
 	{ 
 	    boolean tester = false;
+	    //change this loop to print all odd numbers between 20 and 36
 		for (int i = 10; i<20;i++) {
-			System.out.println("yooolo");
+			System.out.println();
 	    }	
 		System.out.println("Hi my name is Aaron, I weigh 2000 pounds uwu."); 
+		//change the following such that if tester is true print "Shoutout to Match" else if it is false print "Cheddar cheese"
 		if(tester == false){
 		    System.out.println("oogabooga");
 		    }
