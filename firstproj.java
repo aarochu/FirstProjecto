7d73376be96e068a8e89b18057fba25970900db5
Aaron C <babyachu835@gmail.com>
2020-01-19 19:54:24 -0800

    ayeee

--- firstproj.java
+++ firstproj.java
@@ -5,14 +5,15 @@ public class FirstProj
 	{ 
 	    boolean tester = false;
 	    //change this loop to print all odd numbers between 20 and 36
-		for (int i = 10; i<20;i++) {
-			System.out.println();
+		for (int i = 21; i<37;i=i+2) {
+			System.out.println(i);
 	    }	
 		System.out.println("Hi my name is Aaron, I weigh 2000 pounds uwu."); 
 		//change the following such that if tester is true print "Shoutout to Match" else if it is false print "Cheddar cheese"
-		if(tester == false){
-		    System.out.println("oogabooga");
+		if(tester == true){
+		    System.out.println("Shoutout to Match");
+		}	else
+			System.out.println("Cheedar cheese");
 		    }
 		}
 	 
-} 
